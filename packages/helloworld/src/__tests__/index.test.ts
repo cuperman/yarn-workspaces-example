034f9ddd5aad21929915979a0cc9e0a6279f5864
Jeff Cooper <jeff@cuperman.net>
2022-07-15 09:32:12 -0700

    Use momentjs to render pretty timestamps

--- packages/helloworld/src/__tests__/index.test.ts
+++ packages/helloworld/src/__tests__/index.test.ts
@@ -1,8 +1,18 @@
 import { handler } from '../index';
+import lambda from 'aws-lambda';
+
+function buildEvent(overrides?: any): lambda.ScheduledEvent {
+  return {
+    ...overrides,
+  };
+}
 
 describe('index', () => {
-  it('returns Hello, World!', async () => {
-    const response = await handler();
-    expect(response).toEqual('Hello, World!');
+  it('returns greeting and time of day', async () => {
+    const event = buildEvent({
+      time: '2022-07-15T16:26:57.159Z',
+    });
+    const response = await handler(event);
+    expect(response).toEqual("Hello, World!  It's 9:26 AM");
   });
 });
--- packages/helloworld/src/index.ts
+++ packages/helloworld/src/index.ts
@@ -1,3 +1,8 @@
-export async function handler() {
-  return 'Hello, World!';
+import moment from 'moment';
+import lambda from 'aws-lambda';
+
+export async function handler(event: lambda.ScheduledEvent) {
+  const time = moment(event.time);
+
+  return `Hello, World!  It's ${time.format('LT')}`;
 }
